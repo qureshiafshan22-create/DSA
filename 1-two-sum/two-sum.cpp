deb24b148aa2bf0e75f63f7ee41d5a866ab8005c
qureshiafshan22-create <qureshiafshan22@gmail.com>
2026-06-27 11:11:43 +0530

    Time: 71 ms (11.31%) | Memory: 14.1 MB (68.51%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,12 +6,12 @@ public:
 
         for(int i = 0; i < nums.size(); i++) {
 
-          if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;}
+         // if(i > 0 && nums[i] == nums[i - 1]) {
+              //  continue;}
             for(int j = i + 1; j < nums.size(); j++) {
-                if(j>i+1&& nums[j]==nums[j-1]){
-                    continue;
-                }
+           //     if(j>i+1&& nums[j]==nums[j-1]){
+                 //   continue;
+              //  }
 
                 if(nums[i] + nums[j] == target) {
 
