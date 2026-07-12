2d08aead39385f3d352162137e3f5d2bdcd47459
qureshiafshan22-create <qureshiafshan22@gmail.com>
2026-07-12 18:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.66%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         sort(nums.begin(), nums.end());
+         int low=0, mid=0, high=nums.size()-1;
+         while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+
+            }
+            else{
+                mid++;
+            }
+         }
     }
 };
